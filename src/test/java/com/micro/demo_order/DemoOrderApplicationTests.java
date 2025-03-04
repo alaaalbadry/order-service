f3907c9807c0b07814b422d68f041c5aabe964e2
alaaalbadry <albadryalaaa@gmail.com>
2025-03-04 04:26:33 +0200

    create main class and add dependencies to pom.xml

---
+++ src/test/java/com/micro/demo_order/DemoOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.micro.demo_order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoOrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
